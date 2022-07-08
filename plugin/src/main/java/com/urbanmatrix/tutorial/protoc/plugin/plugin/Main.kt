8e0631f9143d4fcb1c12ad90f477b68ae53b8cd3
divyanshu <divyanshu.pundir@gmail.com>
2022-07-08 12:44:42 +0530

    feat: Created plugin

---
+++ plugin/src/main/java/com/urbanmatrix/tutorial/protoc/plugin/plugin/Main.kt
@@ -0,0 +1,51 @@
+package com.urbanmatrix.tutorial.protoc.plugin.plugin
+
+import com.google.protobuf.Descriptors
+import com.google.protobuf.compiler.PluginProtos
+import com.urbanmatrix.tutorial.protoc.plugin.api.CustomInterface
+
+const val PROTO_PACKAGE = "urbanmatrix.tutorial"
+var id = 0
+
+fun main() {
+    val generatorRequest = PluginProtos.CodeGeneratorRequest.parseFrom(System.`in`)
+    val generatorResponse = PluginProtos.CodeGeneratorResponse.newBuilder().apply {
+        supportedFeatures = PluginProtos.CodeGeneratorResponse.Feature.FEATURE_PROTO3_OPTIONAL_VALUE.toLong()
+    }
+
+    generatorRequest.protoFileList
+        .filter { it.`package`.startsWith(PROTO_PACKAGE) }
+        .map { Descriptors.FileDescriptor.buildFrom(it, emptyArray()) }
+        .forEach { fileDescriptor ->
+            fileDescriptor.messageTypes.forEach { descriptor ->
+                val javaFileName = fileDescriptor.options.javaPackage.replace(".", "/") +
+                    "/" + descriptor.name.substringAfterLast(".") + ".java"
+
+                generatorResponse.addFileBuilder().apply {
+                    name = javaFileName
+                    content = CustomInterface::class.java.name + ","
+                    insertionPoint = "message_implements:${descriptor.fullName}"
+                }
+
+                generatorResponse.addFileBuilder().apply {
+                    name = javaFileName
+                    content = generateIdCode()
+                    insertionPoint = "class_scope:${descriptor.fullName}"
+                }
+            }
+        }
+
+    generatorResponse.build().writeTo(System.out)
+}
+
+// Generates a static and class method which returns the fully qualified descriptor name
+fun generateIdCode(): String {
+    return """
+        |private static final int ID = "${++id}";
+        |
+        |@${Override::class.java.name}
+        |public String getId() {
+        |  return ID;
+        |}
+    """.trimMargin()
+}
