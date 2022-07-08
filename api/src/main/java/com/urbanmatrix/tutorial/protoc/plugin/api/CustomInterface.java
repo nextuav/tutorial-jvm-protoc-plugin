fea6f7fc4af08fd75a703ae3a118db3c9aa17998
divyanshu <divyanshu.pundir@gmail.com>
2022-07-08 11:48:56 +0530

    feat: Added CustomInterface

---
+++ api/src/main/java/com/urbanmatrix/tutorial/protoc/plugin/api/CustomInterface.java
@@ -0,0 +1,13 @@
+package com.urbanmatrix.tutorial.protoc.plugin.api;
+
+/**
+ * Interface common to all generated protobuf messages in this tutorial
+ */
+public interface CustomInterface {
+
+    /**
+     * Return the id of the message.
+     * @return The id of the message.
+     */
+    int getId();
+}
